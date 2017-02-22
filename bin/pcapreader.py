051ca76c54aedaba0fe3fe5177ea16024f90f714
Alexandre Dulaunoy <a@foo.be>
2017-02-22 17:23:43 +0100

    fix: pcapreader now works in Python 3

--- bin/pcapreader.py
+++ bin/pcapreader.py
@@ -21,44 +21,50 @@ def init(url, key):
     return PyMISP(url, key, True, 'json')
 
 if __name__ == '__main__':
-	misp = init(misp_url, misp_key)
-	parser = argparse.ArgumentParser(description='TEST')
-	parser.add_argument("-r", "--read", required=True, 
-						help="pcap/dumpcap file that should be read by tshark ")
-	parser.add_argument("-f","--filter", required=False, 
-						help="Prefix that should be skipped (substring)")
-	parser.add_argument("-s","--source", help = "Describe the source of the pcap",
-						required=False, default="undefined")
-	parser.add_argument("-t","--type", required=False,
-						help="Specify the type of sightings: 0=Default,1=False positive", default=0)
-	args = parser.parse_args()
+        misp = init(misp_url, misp_key)
+        parser = argparse.ArgumentParser(description='TEST')
+        parser.add_argument("-r", "--read", required=True,
+                                                help="pcap/dumpcap file that should be read by tshark ")
+        parser.add_argument("-f","--filter", required=False,
+                                                help="Prefix that should be skipped (substring)")
+        parser.add_argument("-s","--source", help = "Describe the source of the pcap",
+                                                required=False, default="undefined")
+        parser.add_argument("-t","--type", required=False,
+                                                help="Specify the type of sightings: 0=Default,1=False positive", default=0)
+        parser.add_argument("-v","--verbose", default=False, action='store_true')
+        args = parser.parse_args()
 
-	p = subprocess.Popen(["tshark","-n","-r", args.read, 
-				      "-Tfields", "-eframe.time_epoch",
-      				  "-Tfields", "-eip.src", 
-					  "-Tfields", "-eip.dst"], 
-					  stdout=subprocess.PIPE, shell=False)
+        p = subprocess.Popen(["tshark","-n","-r", args.read,
+                                      "-Tfields", "-eframe.time_epoch",
+                                  "-Tfields", "-eip.src",
+                                          "-Tfields", "-eip.dst"],
+                                          stdout=subprocess.PIPE, shell=False)
 
 #FIXME Create multiple json docs when large pcaps are used.
 for line in p.stdout:
-	try:
-		line = line[:-1]
-		#Handle nested dissected protocols
-		line = line.replace(',',' ')
-		t = line.split()
-		ts = t[0].split('.')[0]
-		for ip in t[1:]:
-			doc = {}
-			if args.filter is not None and ip.startswith(args.filter[0]):
-				continue
-			doc["values"] = [ip]
-			doc["source"] = args.source
-			doc["type"] = args.type
-			doc["timestamp"] = str(ts)
-			jdoc = json.dumps(doc)
-			res = misp.set_sightings(jdoc)
-			print res
-			#TODO Get always same error from MISP when format is broken and attribute
-			#unknown
-	except IndexError as e:
-		print(e)
+        try:
+                line = line.rstrip()
+                if isinstance(line, bytes):
+                        line=line.decode('ascii')
+
+                #Handle nested dissected nested protocols
+                line = line.replace(',',' ')
+                t = line.split()
+                ts = t[0].split('.')[0]
+                for ip in t[1:]:
+                        doc = {}
+                        if args.filter is not None and ip.startswith(args.filter[0]):
+                                continue
+                        doc["values"] = [ip]
+                        doc["source"] = args.source
+                        doc["type"] = args.type
+                        doc["timestamp"] = str(ts)
+                        jdoc = json.dumps(doc)
+                        if args.verbose:
+                                print ("Input JSON to MISP: {}".format(jdoc))
+                        res = misp.set_sightings(jdoc)
+                        if args.verbose:
+                                print ("MISP response: {}".format(res))
+        except IndexError as e:
+                print(e)
+
