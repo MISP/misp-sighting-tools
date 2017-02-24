a4e21909261cdd7b39b4e4c248f96cf324e2e8a1
Alexandre Dulaunoy <a@foo.be>
2017-02-24 09:33:21 +0100

    add: debug mode added (-d/--debug)
    The debug output gives an output about the processing:
    
    python3 pcapreader.py -r test.cap   --debug
    {'discarded': 0, 'sighted': 8, 'non-sighted': 4}
    
    - discarded means skipped line (not processed)
    - sighted means IP addresses sighted in MISP successfully
    - non-sighted mean IP addresses not-sighted (usually meaning the IP addresses are not in MISP)

--- bin/pcapreader.py
+++ bin/pcapreader.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#TODO refactoring for python3
 
 from pymisp import PyMISP
 from keys import misp_url, misp_key
-import sys
-import pprint
 import argparse
 import subprocess
 import json
@@ -21,50 +18,63 @@ def init(url, key):
     return PyMISP(url, key, True, 'json')
 
 if __name__ == '__main__':
-        misp = init(misp_url, misp_key)
-        parser = argparse.ArgumentParser(description='TEST')
-        parser.add_argument("-r", "--read", required=True,
-                                                help="pcap/dumpcap file that should be read by tshark ")
-        parser.add_argument("-f","--filter", required=False,
-                                                help="Prefix that should be skipped (substring)")
-        parser.add_argument("-s","--source", help = "Describe the source of the pcap",
-                                                required=False, default="undefined")
-        parser.add_argument("-t","--type", required=False,
-                                                help="Specify the type of sightings: 0=Default,1=False positive", default=0)
-        parser.add_argument("-v","--verbose", default=False, action='store_true')
-        args = parser.parse_args()
+	misp = init(misp_url, misp_key)
+	parser = argparse.ArgumentParser(description='TEST')
+	parser.add_argument("-r", "--read", required=True,
+						help="pcap/dumpcap file that should be read by tshark ")
+	parser.add_argument("-f","--filter", required=False,
+						help="Prefix that should be skipped (substring)")
+	parser.add_argument("-s","--source", help = "Describe the source of the pcap",
+						required=False, default="undefined")
+	parser.add_argument("-t","--type", required=False,
+						help="Specify the type of sightings: 0=Default,1=False positive", default=0)
+	parser.add_argument("-v","--verbose", default=False, action='store_true')
+	parser.add_argument("-d","--debug", default=False, action='store_true')
+	args = parser.parse_args()
 
-        p = subprocess.Popen(["tshark","-n","-r", args.read,
-                                      "-Tfields", "-eframe.time_epoch",
-                                  "-Tfields", "-eip.src",
-                                          "-Tfields", "-eip.dst"],
-                                          stdout=subprocess.PIPE, shell=False)
+	p = subprocess.Popen(["tshark","-n","-r", args.read,
+				      "-Tfields", "-eframe.time_epoch",
+      				  "-Tfields", "-eip.src",
+					  "-Tfields", "-eip.dst"],
+					  stdout=subprocess.PIPE, shell=False)
 
 #FIXME Create multiple json docs when large pcaps are used.
+counters = {}
+counters['discarded'] = 0
+counters['sighted'] = 0
+counters['non-sighted'] = 0
 for line in p.stdout:
-        try:
-                line = line.rstrip()
-                if isinstance(line, bytes):
-                        line=line.decode('ascii')
+	try:
+		line = line.rstrip()
+		if isinstance(line, bytes):
+			line=line.decode('ascii')
 
-                #Handle nested dissected nested protocols
-                line = line.replace(',',' ')
-                t = line.split()
-                ts = t[0].split('.')[0]
-                for ip in t[1:]:
-                        doc = {}
-                        if args.filter is not None and ip.startswith(args.filter[0]):
-                                continue
-                        doc["values"] = [ip]
-                        doc["source"] = args.source
-                        doc["type"] = args.type
-                        doc["timestamp"] = str(ts)
-                        jdoc = json.dumps(doc)
-                        if args.verbose:
-                                print ("Input JSON to MISP: {}".format(jdoc))
-                        res = misp.set_sightings(jdoc)
-                        if args.verbose:
-                                print ("MISP response: {}".format(res))
-        except IndexError as e:
-                print(e)
+		#Handle nested dissected nested protocols
+		line = line.replace(',',' ')
+		t = line.split()
+		ts = t[0].split('.')[0]
+		for ip in t[1:]:
+			doc = {}
+			if args.filter is not None and ip.startswith(args.filter[0]):
+				continue
+			doc["values"] = [ip]
+			doc["source"] = args.source
+			doc["type"] = args.type
+			doc["timestamp"] = str(ts)
+			jdoc = json.dumps(doc)
+			if args.verbose:
+				print ("Input JSON to MISP: {}".format(jdoc))
+			res = misp.set_sightings(jdoc)
+			if 'message' in res:
+				if res['message'].find('Could not add Sighting'):
+					counters['non-sighted'] += 1
+				if res['message'].find('sighting successfuly added'):
+					counters['sighted'] +=1
+			if args.verbose:
+				print ("MISP response: {}".format(res))
+	except IndexError as e:
+		print(e)
+		counters['discarded'] += 1
 
+if args.debug:
+	print (counters)
